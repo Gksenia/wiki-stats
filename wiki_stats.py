61c1a86323f3c4480d2dc2696b5c6036868677fe
Gksenia <kglumakova@mail.ru>
2017-03-28 12:08:13 +0300

    end

--- wiki_stats.py
+++ wiki_stats.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 
 import os
 import sys
@@ -17,41 +19,57 @@ import matplotlib.pyplot as plt
 class WikiGraph:
 
     def load_from_file(self, filename):
-        print('Загружаю граф из файла: ' + filename)
+        print('Загружаю граф из файла: ' + filename )
 
         with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
+            (n, _nlinks) = map(int,f.readline().split())
             
             self._titles = []
             self._sizes = array.array('L', [0]*n)
-            self._links = array.array('L', [0]*_nlinks)
             self._redirect = array.array('B', [0]*n)
-            self._offset = array.array('L', [0]*(n+1))
+            self._links = array.array('L', [0]*_nlinks)
 
+            self._offset = array.array('L', [0]*(n+1))
+            self._k = []
             # TODO: прочитать граф из файла
+            for i in range (n):
+                  self._titles.append(f.readline())
+                  (self._sizes,self._redirect,self._k) = map(int,f.readline().split())
+                  for j in range (self._k):
+                       self._links.append(f.readline())
+
+                       if i == 0:
+                           self._offset[i]=0
+                       else:
+                           self._offset[i] = self._offset[i-1] + self._k[i]
 
         print('Граф загружен')
 
     def get_number_of_links_from(self, _id):
-        pass
+        return self._k[_id]
 
     def get_links_from(self, _id):
-        pass
+        return self._links[ self._offset[_id]: self._offset[_id+1]]
 
     def get_id(self, title):
-        pass
+        for _id in range (n) :
+            if title ==  self._titles[_id]:
+                return _id
 
     def get_number_of_pages(self):
-        pass
+        return n
 
     def is_redirect(self, _id):
-        pass
+        if self._redirect[_id] == 0:
+            return False
+        if self._redirect[_id] == 1:
+            return True
 
     def get_title(self, _id):
-        pass
+        return self._titles[_id]
 
     def get_page_size(self, _id):
-        pass
+        return self._sizes[_id]
 
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
